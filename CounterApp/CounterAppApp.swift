9d90e6c8c615601f67610ae4a733e91e102daab4
Pablo Fonseca <pablo.fonsecam@outlook.com>
2023-08-24 12:24:00 -0600

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by Pablo Fonseca Mata on 24/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
